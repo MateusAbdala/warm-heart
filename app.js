5b79166646621b3e19d7150acf0265202d868831
vgabdala <vgabdala@gmail.com>
2017-09-18 17:59:21 +0100

    view entidades done

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 (function() {
-  var app = angular.module('warmheart', ["ngRoute", "Authentication", "Home"]);
+  var app = angular.module('warmheart', ["ngRoute", "Authentication", "Home", "Entidade"]);
 
   //CONTROLLERS--------------------------------------------------------------
-  app.controller('CaridadeController', function() {
-    this.entidades = ent;
-    this.filtro= filt;
-  });
 
   app.controller('GalleryController', function() {
     this.imageIndex = 0;
@@ -17,25 +13,30 @@
 
   //CONFIG ROUTES-----------------------------------------------------------
   app.config(['$routeProvider', function ($routeProvider) {
-    var viewBase = '../view';
+    var fragmentsBase = '../fragments';
     $routeProvider
       .when('/', {
-        templateUrl : viewBase + '/home.html'
+        templateUrl : fragmentsBase + '/home.html'
       })
       .when('/login', {
         module: 'Authentication',
         controller: 'LoginController',
-        templateUrl: viewBase + '/login.html'
+        templateUrl: fragmentsBase + '/login.html'
       })
       .when('/signup', {
         module: 'Authentication',
         controller: 'SignUpController',
-        templateUrl: viewBase + '/signup.html'
+        templateUrl: fragmentsBase + '/signup.html'
       })
       .when('/logout', {
         module: 'Authentication',
         controller: 'LogoutController',
-        templateUrl: viewBase + '/logout.html'
+        templateUrl: fragmentsBase + '/logout.html'
+      })
+      .when('/entidades', {
+        module: 'Entidade',
+        controller: 'EntidadeController',
+        templateUrl: fragmentsBase + '/entidades.html'
       })
   }]);
   
@@ -43,56 +44,15 @@
   app.directive('barranavegacao', function() {
       return {
         restrict: 'E',
-        templateUrl: './directive/barranavegacao.html'
+        templateUrl: './fragments/barranavegacao.html'
       }
     });
 
   app.directive('rodape', function(){
     return {
       restrict: 'E',
-      templateUrl: './directive/rodape.html'
+      templateUrl: './fragments/rodape.html'
     };
   }); 
 
-  //MOCKING - TEMP-----------------------------------------------------------
-  var filt = [{
-      nome: 'Brasil'
-  },{
-      nome: 'São Paulo'
-  }];
-  var ent = [{
-    nome: 'Criança Esperança',
-    descricao: "Criança Esperança é um projeto da Rede Globo em parceria com a UNESCO - Órgão das Nações Unidas para a Educação, a Ciência e a Cultura.",
-    images: [
-      "http://www.unesco.org/new/fileadmin/MULTIMEDIA/FIELD/Brasilia/images/brz_shs_logo_CRIESP_2016.png"
-    ],
-    reviews: [{
-      stars: 5,
-      body: "show!!!",
-      author: "joelma@thomas.com",
-      createdOn: 1397490980837
-    }, {
-      stars: 1,
-      body: "não gostei.",
-      author: "tim@maia.com",
-      createdOn: 1397490980837
-    }]
-  }, {
-    nome: 'Teleton',
-    descricao: "Teleton é uma maratona televisiva brasileira anualmente exibida pelo SBT em prol da Associação de Assistência à Criança Deficiente.",
-    images: [
-      "http://www.sbt.com.br/images/logos_wide/79_480x360.jpg"
-    ],
-    reviews: [{
-      stars: 3,
-      body: "mais ou menos.",
-      author: "JimmyDean@sausage.com",
-      createdOn: 1397490980837
-    }, {
-      stars: 4,
-      body: "curti não, vou dar 4 :p",
-      author: "vanusa@patriota.com",
-      createdOn: 1397490980837
-    }]
-  }];
 })();
---
+++ controllers/entidadesController.js
@@ -0,0 +1,48 @@
+angular.module('Entidade', [])
+
+	.controller('EntidadeController', function() {
+    this.entidades = ent;
+    this.filtro= filt;
+  });
+
+
+  var filt = [{
+      nome: 'Brasil'
+  },{
+      nome: 'São Paulo'
+  }];
+  var ent = [{
+    nome: 'Criança Esperança',
+    descricao: "Criança Esperança é um projeto da Rede Globo em parceria com a UNESCO - Órgão das Nações Unidas para a Educação, a Ciência e a Cultura.",
+    images: [
+      "http://www.unesco.org/new/fileadmin/MULTIMEDIA/FIELD/Brasilia/images/brz_shs_logo_CRIESP_2016.png"
+    ],
+    reviews: [{
+      stars: 5,
+      body: "show!!!",
+      author: "joelma@thomas.com",
+      createdOn: 1397490980837
+    }, {
+      stars: 1,
+      body: "não gostei.",
+      author: "tim@maia.com",
+      createdOn: 1397490980837
+    }]
+  }, {
+    nome: 'Teleton',
+    descricao: "Teleton é uma maratona televisiva brasileira anualmente exibida pelo SBT em prol da Associação de Assistência à Criança Deficiente.",
+    images: [
+      "http://www.sbt.com.br/images/logos_wide/79_480x360.jpg"
+    ],
+    reviews: [{
+      stars: 3,
+      body: "mais ou menos.",
+      author: "JimmyDean@sausage.com",
+      createdOn: 1397490980837
+    }, {
+      stars: 4,
+      body: "curti não, vou dar 4 :p",
+      author: "vanusa@patriota.com",
+      createdOn: 1397490980837
+    }]
+  }];
