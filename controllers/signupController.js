829a1d8ad5a18729c7522be0d12513d73814cba4
vgabdala <vgabdala@gmail.com>
2017-09-25 11:37:30 +0100

    Adding tabs to signup form + few cosmetic changes

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -3,26 +3,34 @@ angular.module('Authentication')
 	.controller('SignUpController',
 		['$rootScope', '$scope', '$timeout', '$location', 'SignUpClient', 'AuthenticationClient',
 		function ($rootScope, $scope, $timeout, $location, SignUpClient, AuthenticationClient) {
-			$scope.signup = function () {
-	        $scope.dataLoading = true;
 
-	        SignUpClient.SignUp($scope.username, $scope.password,
-	        	function(response) {
-	            if(response.data.success) {
-	            	AuthenticationClient.Login($scope.username, $scope.password, function(authResponse) {
-		            	if(authResponse.data.success) {
-			            	AuthenticationClient.SetCredentials($scope.username, authResponse.data.token);
-			            	$rootScope.$broadcast('userLoggedIn');
-			            	$location.path('/');
-			            } else {
-			              $scope.error = authResponse.data.message;
-			              $scope.dataLoading = false;
-			            }
-				        });
-	            } else {
-	              $scope.error = response.data.message;
-	              $scope.dataLoading = false;
-	            }
-	        });
-	    };
+			$scope.handleTabs = function (selectedTab) {
+				if(selectedTab.id == 'tabUsuario'){
+					$scope.tabEntidadeSelected = false;
+				} else if (selectedTab.id == 'tabEntidade'){
+					$scope.tabEntidadeSelected = true;
+				}
+			}
+
+			$scope.signup = function () {
+				$scope.dataLoading = true;
+				SignUpClient.SignUp($scope.username, $scope.password, function(response) {
+					if(response.data.success) {
+						AuthenticationClient.Login($scope.username, $scope.password, function(authResponse) {
+							if(authResponse.data.success) {
+								AuthenticationClient.SetCredentials($scope.username, authResponse.data.token);
+								$rootScope.$broadcast('userLoggedIn');
+								$location.path('/');
+							} else {
+								$scope.error = authResponse.data.message;
+								$scope.dataLoading = false;
+							}
+						});
+					} else {
+						$scope.error = response.data.message;
+						$scope.dataLoading = false;
+					}
+				});
+	    	};
 		}]);
+
