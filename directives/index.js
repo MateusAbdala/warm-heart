faf12451e7fc09aa1a87cdefbce9bedec54be099
MateusAbdala <mateus.gui.abdala@gmail.com>
2017-09-28 22:13:03 -0300

    navbar pageController added

--- directives/index.js
+++ directives/index.js
@@ -3,7 +3,19 @@ angular.module('warmheart')
 .directive('barranavegacao', function() {
   return {
     restrict: 'E',
-    templateUrl: './fragments/barranavegacao.html'
+    templateUrl: './fragments/barranavegacao.html',
+    controller: function() {
+      this.page = 1;
+
+      this.isSet = function(checkPage) {
+        return this.page === checkPage;
+      };
+
+      this.setPage = function(activePage) {
+        this.page = activePage;
+      };
+    },
+    controllerAs: "page"
   };
 })
 
